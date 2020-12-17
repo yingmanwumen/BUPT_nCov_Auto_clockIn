d7c6e805a545f54cb24f1954a06ac27f9a5f05a4
yingmanwumen <2302753939@qq.com>
2020-12-17 16:32:44 +0800

    newest

---
+++ auto.py
@@ -0,0 +1,102 @@
+import requests
+import os
+import time
+import datetime
+import json
+from config import *
+from conf import USERNAME, PASSWORD
+
+
+class Automatic(object):
+	"""
+	自动填报脚本
+	"""
+	def __init__(self, username="", password=""):
+		"""
+		Init the object and do some work
+		"""
+		self.cookies = None
+		self.username = username
+		self.password = password
+		self.today = datetime.date.today()
+		self.date = "%4d%02d%02d" % (self.today.year, self.today.month,
+			self.today.day)
+		self.main()
+
+
+	def authorize(self):
+		"""
+		Authorize
+		"""
+		# username or password is NULL, exit directly
+		if (self.username == "" or self.password == ""):
+			print("username or password is NULL!")
+			os._exit()
+
+		# send POST request
+		data = {
+			"username": self.username,
+			"password": self.password
+		}
+
+		try:
+			print("Start Authorize for %s..." % self.username)
+			self.cookies = requests.post(url=LOGIN_URL, data=data).cookies
+
+		except Exception as e:
+			print("Error Happen when LOGIN:", e)
+			raise
+
+		else:
+			print("Successfully Authorized\n")
+
+
+	def post(self):
+		"""
+		Send POST request and finish
+		"""
+		data = DATA
+		data["created"] = round(time.time())
+		data["date"] = self.date
+		data["area"] = AREA
+		data["city"] = CITY
+		data["province"] = PROVINCE
+
+		try:
+			print("Form:")
+			for item in data:
+				print("\t" + item + ":", data[item])
+
+			print("Cookies:")
+			for item in self.cookies:
+				print("\t" + item.name + "=",item.value)
+
+			print("\nPost...")
+			res = requests.post(url=FORM_URL, data=data, cookies=self.cookies)
+
+		except Exception as e:
+			print("Error Happen when POST Msg:", e)
+			raise
+
+		else:
+			print("Successfully POSTed")
+			return json.loads(res.text)
+
+
+	def main(self):
+		"""
+		Main func
+		"""
+		print("==========================")
+		print("Date: %4d-%02d-%02d" % (self.today.year,
+			self.today.month, self.today.day))
+		print("--------------------------")
+
+		self.authorize()  # Get cookies
+		res = self.post()
+		print(res['m'] + "\n")
+
+
+
+if __name__ == '__main__':
+	Automatic(username=USERNAME, password=PASSWORD)
---
+++ conf.py
@@ -0,0 +1,2 @@
+USERNAME = "Your Student Number"  # 学号
+PASSWORD = "Your password"  # 信息门户密码
